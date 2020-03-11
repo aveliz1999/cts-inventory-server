0a4f077afde8e9c8f6dd128862675adce0a61999
aveliz1999 <github@veliz99.com>
2020-03-11 15:50:44 -0400

    Add redis cookie session to app

--- app.ts
+++ app.ts
@@ -2,8 +2,33 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import session, {SessionOptions} from 'express-session';
+import redis from 'redis';
+import {session as sessionConfig, redis as redisConfig} from './config';
+import connectRedis from 'connect-redis';
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient(redisConfig)
 
 const app = express();
+
+const sessionOptions: SessionOptions = {
+    cookie: {
+        maxAge: 36,
+        httpOnly: false
+    },
+    name: 'sessId',
+    saveUninitialized: true,
+    secret: sessionConfig.sessionSecrets,
+    resave: true,
+    rolling: true,
+    store: new RedisStore({client: redisClient})
+}
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1);
+    sessionOptions.cookie.secure = true;
+}
+app.use(session(sessionOptions));
 
 app.use(logger('dev'));
 app.use(express.json());
--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,7 @@
 import databaseConfig from './database.json';
 import passwordConfig from './passwords.json';
+import sessionConfig from './session.json';
+import redisConfig from './redis.json';
 import {Dialect, OperatorsAliases} from "sequelize";
 
 const env = process.env.NODE_ENV || 'development';
@@ -18,3 +20,16 @@ type PasswordConfig = {
     saltRounds: number
 }
 export const passwords: PasswordConfig = passwordConfig[env];
+
+type SessionConfig = {
+    sessionSecrets: string[]
+}
+export const session: SessionConfig = sessionConfig[env];
+
+type RedisConfig = {
+    host: string,
+    port: number,
+    password?: string,
+    db?: string | number
+}
+export const redis: RedisConfig = redisConfig[env];
