a550f4ecebf7f9b511a1794456a6d9cfa6b35e1a
aveliz1999 <github@veliz99.com>
2020-03-13 14:11:41 -0400

    Add authenticated middleware

--- routes/users.ts
+++ routes/users.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import {getUser, createUser, login} from '../controllers/users';
+import authenticated from "../middleware/authenticated";
 
 const router = express.Router();
 
 router.post('/', createUser);
 router.post('/login', login);
+
+router.use(authenticated);
+
 router.get('/:id', getUser);
 
 export default router;
