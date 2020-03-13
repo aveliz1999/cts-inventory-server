f9961e696ae7b812276d6ad6249784549c483e0a
aveliz1999 <github@veliz99.com>
2020-03-13 03:44:59 -0400

    Add user logging in tests

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -129,7 +129,7 @@ describe('Users routes/controllers', function () {
                 } = (await chai.request(app)
                     .post('/users')
                     .send({
-                        username: username,
+                        username,
                         name: faker.name.findName(faker.name.firstName(), faker.name.lastName()).substring(0, 32),
                         password: faker.internet.password(50, false, /[a-zA-Z0-9 `~!@#$%^&*()\-_+=[\]{};:'"<>,./?\\]+/)
                     })).body;
@@ -141,12 +141,118 @@ describe('Users routes/controllers', function () {
                 const {message}: { message: string } = (await chai.request(app)
                     .post('/users')
                     .send({
-                        username: username,
+                        username,
                         name: faker.name.findName(faker.name.firstName(), faker.name.lastName()).substring(0, 32),
                         password: faker.internet.password(50, false, /[a-zA-Z0-9 `~!@#$%^&*()\-_+=[\]{};:'"<>,./?\\]+/)
                     })).body;
                 assert.equal(message, "An account with that username already exists.");
             });
-        })
+        });
     });
+
+    describe('Logging in', function () {
+        describe('Username', function () {
+            it('Fails when username is not present', async function () {
+                const {message}: { message: string } = (await chai.request(app)
+                    .post('/users/login')
+                    .send({})).body;
+                assert.equal(message, "child \"username\" fails because [\"username\" is required]");
+
+            });
+            it('Fails when username is empty', async function () {
+                const {message}: { message: string } = (await chai.request(app)
+                    .post('/users/login')
+                    .send({username: ""})).body;
+                assert.equal(message, "child \"username\" fails because [\"username\" is not allowed to be empty]");
+
+            });
+            it('Fails when username is too long', async function () {
+                const {message}: { message: string } = (await chai.request(app)
+                    .post('/users/login')
+                    .send({username: faker.internet.userName(faker.name.firstName(), faker.name.lastName()).padEnd(17, '0123456789')})).body;
+                assert.equal(message, "child \"username\" fails because [\"username\" length must be less than or equal to 16 characters long]");
+            });
+        });
+        describe('Password', function () {
+            it('Fails when password is not present', async function () {
+                const {message}: { message: string } = (await chai.request(app)
+                    .post('/users/login')
+                    .send({
+                        username: faker.internet.userName(faker.name.firstName(), faker.name.lastName()).substring(0, 16),
+                        name: faker.name.findName(faker.name.firstName(), faker.name.lastName()).substring(0, 32)
+                    })).body;
+                assert.equal(message, "child \"password\" fails because [\"password\" is required]");
+            });
+            it('Fails when password is empty', async function () {
+                const {message}: { message: string } = (await chai.request(app)
+                    .post('/users/login')
+                    .send({
+                        username: faker.internet.userName(faker.name.firstName(), faker.name.lastName()).substring(0, 16),
+                        name: faker.name.findName(faker.name.firstName(), faker.name.lastName()).substring(0, 32),
+                        password: ""
+                    })).body;
+                assert.equal(message, "child \"password\" fails because [\"password\" is not allowed to be empty]");
+
+            });
+            it('Fails when password is too long', async function () {
+                const {message}: { message: string } = (await chai.request(app)
+                    .post('/users/login')
+                    .send({
+                        username: faker.internet.userName(faker.name.firstName(), faker.name.lastName()).substring(0, 16),
+                        name: faker.name.findName(faker.name.firstName(), faker.name.lastName()).substring(0, 32),
+                        password: faker.internet.password(51, false, /[a-zA-Z0-9 `~!@#$%^&*()\-_+=[\]{};:'"<>,./?\\]+/)
+                    })).body;
+                assert.equal(message, "child \"password\" fails because [\"password\" length must be less than or equal to 50 characters long]");
+            });
+            it('Fails when password has invalid characters', async function () {
+                const password = faker.internet.password(50, false, /[^a-zA-Z0-9 `~!@#$%^&*()\-_+=[\]{};:'"<>,./?\\]+/);
+                const {message}: { message: string } = (await chai.request(app)
+                    .post('/users/login')
+                    .send({
+                        username: faker.internet.userName(faker.name.firstName(), faker.name.lastName()).substring(0, 16),
+                        name: faker.name.findName(faker.name.firstName(), faker.name.lastName()).substring(0, 32),
+                        password
+                    })).body;
+                assert.equal(message, `child "password" fails because ["password" with value "${password}" fails to match the required pattern: /[a-zA-Z0-9 \`~!@#$%^&*()\\-_+=[\\]{};:'"<>,./?\\\\]+/]`);
+            });
+        });
+        describe('Valid data', function () {
+            const username = faker.internet.userName(faker.name.firstName(), faker.name.lastName()).substring(0, 16);
+            const password = faker.internet.password(50, false, /[a-zA-Z0-9 `~!@#$%^&*()\-_+=[\]{};:'"<>,./?\\]+/);
+            before('Register user', async function () {
+                const user: User = await User.findOne({
+                    where: {
+                        username
+                    }
+                });
+                if (user) {
+                    await user.destroy();
+                }
+                await chai.request(app)
+                    .post('/users')
+                    .send({
+                        username,
+                        name: faker.name.findName(faker.name.firstName(), faker.name.lastName()).substring(0, 32),
+                        password
+                    });
+            });
+
+            it('Logging in succeeds with valid data', async function () {
+                const user: {
+                    id: number,
+                    username: string,
+                    name: string,
+                    updatedAt: Date,
+                    createdAt: Date
+                } = (await chai.request(app)
+                    .post('/users/login')
+                    .send({
+                        username,
+                        password
+                    })).body;
+                assert.exists(user);
+                assert.equal(username, user.username);
+            });
+        })
+    })
 });
